269bbf9af99e1a606f59294b3754fc5289c287fe
ManfredSteyer <manfred.steyer@gmx.net>
2017-09-23 20:43:01 +0200

    refactoring, variable names

--- shell/src/client/app/app.component.ts
+++ shell/src/client/app/app.component.ts
@@ -19,34 +19,29 @@ export class AppComponent implements AfterViewInit, OnDestroy   {
 
   // Placeholder (div) with Handle vc
   @ViewChild("vc", {read: ViewContainerRef}) 
-  vc: ViewContainerRef;
+  viewContainer: ViewContainerRef;
 
-  compRef: ComponentRef<{}>;
+  componentRef: ComponentRef<{}>;
 
   ngAfterViewInit() {
-    console.debug('go');
-    this.loader.load('http://127.0.0.1:8080/plugin-rollup.umd.js#SampleModule').then((factory) => {
+    this.loader.load('http://127.0.0.1:8080/plugin-rollup.umd.js#SampleModule').then((moduleFactory) => {
       
-      console.debug('loaded');
-      const module = factory.create(this.injector);
+      let moduleRef = moduleFactory.create(this.injector);
+      let module = moduleRef.instance;
 
-      let compType = module.instance.constructor.decorators[0].args[0].exports.find( (e: Function) => e.name == 'DashBoardComponent');
-
-      console.debug('module.instance', compType);
-      const r = module.componentFactoryResolver;
+      let compType = module.constructor.decorators[0].args[0].exports.find( (e: Function) => e.name == 'DashBoardComponent');
       
-      const cmpFactory = r.resolveComponentFactory(compType);
-      let compRef = this.vc.createComponent(cmpFactory, null, this.injector);
-      let comp: any = compRef.instance;
-      console.debug('compRef', compRef);
-      console.debug('comp', comp);
+      let componentFactoryResolver = moduleRef.componentFactoryResolver;
+      let componentFactory = componentFactoryResolver.resolveComponentFactory(compType);
+      let componentRef = this.viewContainer.createComponent(componentFactory, null, this.injector);
+      let comp: any = componentRef.instance;
 
-      this.compRef = compRef;
+      this.componentRef = componentRef;
     });
   }
 
   ngOnDestroy() {
-    this.compRef.destroy();
+    this.componentRef.destroy();
   }
 
 
